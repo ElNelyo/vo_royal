ff32ec82d2597171cd26c56099efa12442e26de4
Unknown <charly.strn@gmail.com>
2019-02-07 15:09:20 +0100

    Install @vue/cli

---
+++ app/src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+
+        <table>
+            <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Famille</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="result in results">
+                <td>{{result.nom}}</td>
+                <td>{{result.prenom}}</td>
+                <td>{{result.famille}}</td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
+</template>
